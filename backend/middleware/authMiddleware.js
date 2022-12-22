05d148968044a3ef094fcaa5f799742296ef0cda
josefdzbeta <jafb19@outlook.com>
2022-12-22 14:51:15 +0100

    auth JWT finished

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,12 +1,29 @@
-const checkAuth = (req, res, next) =>{ //Next detiene la ejecución del código y salta al siguiente middleware si no detecta 
+import jwt from "jsonwebtoken";
+import Veterinario from "../models/Veterinario.js";
 
+const checkAuth = async (req, res, next) =>{ //Next detiene la ejecución del código y salta al siguiente middleware si no detecta 
+
+    let token;
     //Comprobar JWT
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        console.log('Si tiene el token con Bearer')
+        try {
+            token = req.headers.authorization.split(' ')[1]; //obtenemos sólo el jwt
+            const decoded = jwt.verify(token, process.env.JWT_SECRET); //verify toma diferentes parámetros
+            
+            //req.veterinario crea una sesión con la información del veterinario
+            req.veterinario = await Veterinario.findById(decoded.id).select('-password -token -confirmado');
+
+            return next();
+        } catch (error) {
+            const e = new Error('Token no válido');
+            return res.status(403).json({msg: e.message});
+        }
     }
-    //Si no está el token lanza el siguiente error
-    const error = new Error('Token no válido o inexistente')
-    res.status(403).json({msg: error.message})
+    if (!token) {
+        const error = new Error('Token no válido o inexistente');
+        res.status(403).json({msg: error.message});
+    }
+   
     next();
 }
 
