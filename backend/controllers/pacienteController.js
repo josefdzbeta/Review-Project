bbbfdbac963e1dfe6c2ea5ed701503bdd10203f7
josefdzbeta <jafb19@outlook.com>
2022-12-23 13:25:37 +0100

    find specific user

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -12,13 +12,47 @@ const agregarPaciente = async (req, res) => {
   }
 };
 
-const obtenerPaciente = async (req, res) =>{
+const obtenerPacientes = async (req, res) =>{
     const pacientes = await Paciente.find().where('veterinario').equals(req.veterinario);
     res.json(pacientes)
 } 
 
+const obtenerPaciente = async (req, res) =>{
+  const {id} = req.params;
+  const paciente = await Paciente.findById(id);
+
+  /*console.log(paciente.veterinario._id)
+  console.log(req.veterinario._id)
+
+  tienen el mismo valor*/
+
+  if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()){ //Los convertimos a String para que no sean Objects Id
+    return res.json({msg: 'Acción no válida'}) //Cuando se comparen los ids en mongodb. Es mejor convertirlos a String
+  }
+  if(paciente){
+    res.json(paciente);
+  }
+}
+const actualizarPaciente = async (req, res) =>{
+  const {id} = req.params;
+  const paciente = await Paciente.findById(id);
+
+  if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()){ //Los convertimos a String para que no sean Objects Id
+    return res.json({msg: 'Acción no válida'}) //Cuando se comparen los ids en mongodb. Es mejor convertirlos a String
+  }
+  if(paciente){
+    res.json(paciente);
+  }
+}
+const eliminarPaciente = async (req, res) =>{
+
+}
+
 
 export {
     agregarPaciente,
-    obtenerPaciente
+    obtenerPacientes,
+    obtenerPaciente,
+    actualizarPaciente,
+    eliminarPaciente
 }
--- backend/routes/PacienteRoutes.js
+++ backend/routes/PacienteRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { agregarPaciente, obtenerPaciente } from "../controllers/pacienteController.js";
+import { agregarPaciente, obtenerPacientes, obtenerPaciente, actualizarPaciente, eliminarPaciente } from "../controllers/pacienteController.js";
 import checkAuth from '../middleware/authMiddleware.js'
 const router = express.Router();
 
 
    //Para poder agregar un usuario, es necesario tener una cuenta 
-router.route("/").post(checkAuth, agregarPaciente).get(checkAuth, obtenerPaciente);
+router.route("/").post(checkAuth, agregarPaciente).get(checkAuth, obtenerPacientes);
 
-
+router.route("/:id")
+   .get(checkAuth, obtenerPaciente)
+   .put(checkAuth, actualizarPaciente)
+   .delete(checkAuth, eliminarPaciente);
 export default router;
