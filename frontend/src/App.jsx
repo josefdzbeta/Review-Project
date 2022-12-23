cb6cec16fa33988a7b1c33fdbb775a313874613f
josefdzbeta <jafb19@outlook.com>
2022-12-23 15:30:42 +0100

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import AuthLayout from "./layout/AuthLayout";
 import Login from "./paginas/Login";
+import Registrar from "./paginas/Registrar";
+import ResetPassword from "./paginas/ResetPassword";
+import ConfirmarCuenta from "./paginas/ConfirmarCuenta";
 function App() {
 //Route agrupa todos los componentes que pertenezcan a AuthLayout
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<AuthLayout/>}> //Cargar componente
           <Route index element={<Login />}/> {/* Index define lo que es el primer componente*/ }
+          <Route path="registrar" element={<Registrar />}/> 
+          <Route path="reset-password" element={<ResetPassword />}/> 
+          <Route path="confirmar/:id" element={<ConfirmarCuenta />}/> {/*:id ruta dinámica */}
           
         </Route> 
       </Routes>
