c810773160299be2db8d341c9e751f405c415989
josefdzbeta <jafb19@outlook.com>
2022-12-27 13:27:54 +0100

    Error alerts added to sign up form

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,17 +1,50 @@
 import { Link } from "react-router-dom"
 import { useState } from "react";
+import axios from 'axios'
+import Alerta from "../components/Alerta";
 const Registrar = () => {
 
   const [nombre, setNombre] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repetirPassword, setRepetirPassword] = useState('');
+  
+  const [alerta, setAlerta] = useState({})
 
+  const handleSubmit = async e =>{
+    e.preventDefault();
+    if([nombre, email, password, repetirPassword].includes('')){
+      setAlerta({msg: 'Hay campos vacíos', error: true })
+      return; 
+    }
+    if(password !== repetirPassword){
+      setAlerta({msg: 'Las contraseñas no son iguales', error: true })
+      return; 
+    }
+    if(password.length < 6){
+      setAlerta({msg: 'Introduce un mínimo de 6 caracteres', error: true })
+      return;
+    }
+    setAlerta({}); //Vaciamos alerta si no hay errores
+
+    //Crear usuario en la API
+    try {
+      const url = 'http://localhost:5173/api/veterinarios'
+      const respuesta = await axios.post(url, {nombre, email, password}) //Creamos objeto con los valores del formulario
+      console.log(respuesta);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  //Cargar el componente de manera condicional
+  const {msg} = alerta 
+  
   return (
     <>
       <h1 className="text-indigo-600 font-black text-6xl">Crea tu cuenta y Administra <span className="text-black">tus Pacientes</span> </h1>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form >
+        {msg && <Alerta alerta ={alerta} />}
+        <form onSubmit={handleSubmit}>
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold">Nombre</label>
             <input type="text" placeholder="Tu Nombre" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={nombre} onChange={ e => setNombre(e.target.value)}/>
