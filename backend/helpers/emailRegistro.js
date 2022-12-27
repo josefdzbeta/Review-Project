445d089cc015e0196d33cd23345679f97f563fbd
josefdzbeta <jafb19@outlook.com>
2022-12-27 14:50:01 +0100

    send verification email

---
+++ backend/helpers/emailRegistro.js
@@ -0,0 +1,12 @@
+import nodemailer from "nodemailer";
+
+const emailRegistro = () =>{
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_USER,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
+        }
+      });
+}
