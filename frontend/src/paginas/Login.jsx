263f98a9f9d4a751cc9ec8ee8d6a31e91072b02f
josefdzbeta <jafb19@outlook.com>
2022-12-28 13:33:55 +0100

    auth admin page

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -10,6 +10,7 @@ const Login = () => {
   const [password, setPassword] = useState('')
   const [alerta, setAlerta] = useState({})
 
+  const {setAuth} = useAuth()
   const navigate = useNavigate()
 
   const handleSubmit = async (e) =>{
@@ -25,9 +26,11 @@ const Login = () => {
     }
     try {
       const {data} = await clienteAxios.post('/veterinarios/login', {email, password})
+     
       localStorage.setItem('token', data.token)
-
+      setAuth(data)
       navigate('/admin')
+
     } catch (error) {
       setAlerta({
         msg: error.response.data.msg,
