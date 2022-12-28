df286d85ac806df6fd4b3f1dbece70a3fb7bc874
josefdzbeta <jafb19@outlook.com>
2022-12-28 02:03:28 +0100

    reset password email

---
+++ frontend/src/paginas/NuevaPass.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react"
+const NuevaPass = () => {
+  return (
+    <h1>NuevaPass</h1>
+  )
+}
+
+export default NuevaPass
--- frontend/src/paginas/ResetPassword.jsx
+++ frontend/src/paginas/ResetPassword.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/axios";
 import { Link } from "react-router-dom";
 const ResetPassword = () => {
+
+  const [email, setEmail] = useState('') //Escuchar cambios en el input de email
+  const [alerta, setAlerta] = useState({}) //Alerta
+  
+  
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    if(email === '' || email.length < 6){
+      setAlerta({msg: 'El email es obligatorio',error: true})
+      return
+    }
+
+    try {
+      const {data} = await clienteAxios.post('/veterinarios/reset-password', {email}) //Data se obtiene de la respuesta de axios
+      setAlerta({
+        msg: data.msg //Accedemos al mensaje de backend
+      })
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true
+      })
+    }
+
+  }
+
+  const {msg} = alerta
+
   return (
     <>
       <h1 className="text-indigo-600 font-black text-6xl">Reiniciar <span className="text-black">contraseña</span> </h1>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form >
+        {msg && <Alerta alerta={alerta} />}
+        <form onSubmit={handleSubmit}>
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold">Email</label>
-            <input type="Email" placeholder="Tu Email" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" />
+            <input type="Email" placeholder="Tu Email" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={email} onChange={e=>setEmail(e.target.value)}/>
           </div>
           <input type="submit" value="Enviar Instrucciones" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto" />
         </form>
