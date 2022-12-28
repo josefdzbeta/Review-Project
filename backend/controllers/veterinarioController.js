e6f3e92ab5223b1d39e785eccf38d3ccf80a0e39
josefdzbeta <jafb19@outlook.com>
2022-12-28 10:35:18 +0100

    minor object value change

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -35,7 +35,7 @@ const registrar =  async (req, res)=>{
 }
 const perfil = (req, res)=>{
     const {veterinario} = req
-    res.json({perfil: veterinario})
+    res.json(veterinario)
 }
 
 const confirmar = async (req, res)=>{
