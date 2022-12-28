24192524c234a9b4f1662f9c85590dad11815a25
josefdzbeta <jafb19@outlook.com>
2022-12-29 00:04:57 +0100

    new pages for editing profile and updating password

---
+++ frontend/src/paginas/CambiarPassword.jsx
@@ -0,0 +1,13 @@
+import AdminNav from "../components/AdminNav"
+
+const cambiarPassword = () => {
+  return (
+    <>
+        <AdminNav />
+        <h2 className="font-black text-3xl text-center mt-10">Cambiar Contraseña</h2>
+        <p className="text-xl mt-5 mb-10 text-center">Modifica tu <span className="text-indigo-600 font-bold">contraseña</span></p>
+    </>
+  )
+}
+
+export default cambiarPassword
---
+++ frontend/src/paginas/EditarPerfil.jsx
@@ -0,0 +1,13 @@
+import AdminNav from "../components/AdminNav"
+
+const EditarPerfil = () => {
+  return (
+    <>
+        <AdminNav />
+        <h2 className="font-black text-3xl text-center mt-10">Editar Perfil</h2>
+        <p className="text-xl mt-5 mb-10 text-center">Modifica tu <span className="text-indigo-600 font-bold">Información</span></p>  
+    </>
+  )
+}
+
+export default EditarPerfil
