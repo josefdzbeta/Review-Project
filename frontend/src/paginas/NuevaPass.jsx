6e3067636871226f8f3bc23c73d699ab2ab27453
josefdzbeta <jafb19@outlook.com>
2022-12-28 02:43:16 +0100

    sending resetpassword email

--- frontend/src/paginas/NuevaPass.jsx
+++ frontend/src/paginas/NuevaPass.jsx
@@ -1,8 +1,84 @@
-import { useState } from "react"
+import { useState, useEffect} from "react"
+import { useParams, Link } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/axios";
+
 const NuevaPass = () => {
-  return (
-    <h1>NuevaPass</h1>
-  )
+    const [password, setPassword] = useState('');
+    const [alerta, setAlerta] = useState({});
+    const [tokenValido, setTokenValido] = useState(false);
+
+    //Obtenemos el token de la URL
+    const params = useParams()
+    const {token} = params
+
+    useEffect(() =>{
+        const comprobarToken = async () =>{
+            try {
+                await clienteAxios(`/veterinarios/reset-password/${token}`)
+                setAlerta({
+                    msg: 'Introduce tu nueva contraseña'
+                })
+                setTokenValido(true)
+            } catch (error) {
+               setAlerta({
+                msg: 'Hubo un error con el enlace',
+                error: true
+               })
+            }
+        }
+        comprobarToken();
+    }, []) //Se ejecuta una sóla vez cuando carga el componente
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+
+        if(password.length<6){
+            setAlerta({
+                msg: 'La contraseña debe tener al menos 6 caracteres',
+                error: true
+            })
+            return 
+        }
+        try {
+            const url = `/veterinarios/reset-password/${token}`
+            const {data} = await clienteAxios.post(url,{password})
+
+            setAlerta({
+                msg: data.msg
+            })
+            
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+    }
+    
+    const {msg} = alerta
+    return (
+        <>
+            <h1 className="text-indigo-600 font-black text-6xl">Reestablece tu contraseña y no pierdas acceso<span className="text-black">a tus pacientes</span> </h1>
+            <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+                {msg && <Alerta alerta={alerta} />}
+                {tokenValido && ( //Si el token es válido retorna el formulario, sino no aparece nada
+                    <form onSubmit={handleSubmit}>
+                    
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">Nueva Contraseña</label>
+                        <input type="password" placeholder="Tu contraseña" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={password} onChange={e => setPassword(e.target.value)} />
+                    </div>
+                    <input type="submit" value="Guardar nueva contraseña" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto" />
+                </form>
+
+                )}
+                
+            </div>
+
+        </>
+
+    )
 }
 
 export default NuevaPass
