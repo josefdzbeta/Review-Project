ab479d50b87b7907253775f3cad3d46b363181cc
josefdzbeta <jafb19@outlook.com>
2022-12-28 12:58:58 +0100

    new header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
 
 const Header = () => {
+
+    const {cerrarSesion} = useAuth()
   return (
-    <div>Header</div>
+    <header className="py-10 bg-indigo-600">
+        <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
+            <h1 className="font-bold text-2xl text-indigo-200 text-center">Administrador de Pacientes de {''} <span className="text-white font-black">Veterinaria</span></h1>
+            <nav className="flex flex-col items-center lg:flex-row gap-4 lg:mt-0">
+                <Link to="/admin text-white text-sm uppercase font-bold">Pacientes</Link>
+                <Link to="/admin text-white text-sm uppercase font-bold">Perfil</Link>
+                <button type="button" className="text-white text-sm uppercase font-bold" onClick={cerrarSesion}>Cerrar Sesión</button>
+            </nav>
+        </div>
+    </header>
   )
 }
 
