3e956568e8189bd17ce73d96c7f50159493e060e
josefdzbeta <jafb19@outlook.com>
2022-12-28 01:16:55 +0100

    urlBASE axios config sign up

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,235 +1,83 @@
-// import { Link } from "react-router-dom"
-// import { useState } from "react";
-// import clienteAxios from "../config/axios";
-// import Alerta from "../components/Alerta";
-// const Registrar = () => {
-
-//   const [nombre, setNombre] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [repetirPassword, setRepetirPassword] = useState('');
-
-//   const [alerta, setAlerta] = useState({})
-
-//   const handleSubmit = async e => {
-//     e.preventDefault();
-//     if ([nombre, email, password, repetirPassword].includes('')) {
-//       setAlerta({ msg: 'Hay campos vacíos', error: true })
-//       return;
-//     }
-//     if (password !== repetirPassword) {
-//       setAlerta({ msg: 'Las contraseñas no son iguales', error: true })
-//       return;
-//     }
-//     if (password.length < 6) {
-//       setAlerta({ msg: 'Introduce un mínimo de 6 caracteres', error: true })
-//       return;
-//     }
-//     setAlerta({}); //Vaciamos alerta si no hay errores
-
-//     //Crear usuario en la API
-//     try {
-//       await clienteAxios.post('/veterinarios', { nombre, email, password }) //Creamos objeto con los valores del formulario
-//       setAlerta({
-//         msg: 'Usuario creado correctamente, revisa tu email',
-//         error: false
-//       });
-//     } catch (error) {
-//       setAlerta({
-//         msg: error.response.data.msg, //Obtener acceso a los mensajes que hemos declarado en el backend
-//         error: true
-//       })
-//     }
-//   }
-//   //Cargar el componente de manera condicional
-//   const { msg } = alerta
-
-//   return (
-//     <>
-//       <h1 className="text-indigo-600 font-black text-6xl">Crea tu cuenta y Administra <span className="text-black">tus Pacientes</span> </h1>
-//       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-//         {msg && <Alerta alerta={alerta} />}
-//         <form onSubmit={handleSubmit}>
-//           <div className="my-5">
-//             <label className="uppercase text-gray-600 block text-xl font-bold">Nombre</label>
-//             <input type="text" placeholder="Tu Nombre" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={nombre} onChange={e => setNombre(e.target.value)} />
-//           </div>
-//           <div className="my-5">
-//             <label className="uppercase text-gray-600 block text-xl font-bold">Email</label>
-//             <input type="email" placeholder="Email" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={email} onChange={e => setEmail(e.target.value)} />
-//           </div>
-//           <div className="my-5">
-//             <label className="uppercase text-gray-600 block text-xl font-bold">Contraseña</label>
-//             <input type="password" placeholder="Tu contraseña" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={password} onChange={e => setPassword(e.target.value)} />
-//           </div>
-//           <div className="my-5">
-//             <label className="uppercase text-gray-600 block text-xl font-bold">Repetir contraseña</label>
-//             <input type="password" placeholder="Repetir contraseña" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={repetirPassword} onChange={e => setRepetirPassword(e.target.value)} />
-//           </div>
-//           <input type="submit" value="Crear Cuenta" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto" />
-
-//         </form>
-//         <nav className="mt-10 lg:flex lg:justify-between">
-//           <Link className="block text-center my-5 text-gray-500" to="/">¿Ya tienes una cuenta? Inicia Sesión</Link>
-//           <Link className="block text-center my-5 text-gray-500" to="/reset-password">Olvidé mi contraseña</Link>
-//         </nav>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default Registrar
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import Alerta from '../components/Alerta';
-import clienteAxios from '../config/axios';
-
+import { Link } from "react-router-dom"
+import { useState } from "react";
+import clienteAxios from "../config/axios";
+import Alerta from "../components/Alerta";
 const Registrar = () => {
-    const [ nombre, setNombre ] = useState('')
-    const [ email, setEmail ] = useState('')
-    const [ password, setPassword ] = useState('')
-    const [ repetirPassword, setRepetirPassword ] = useState('')
 
-    const [alerta, setAlerta] = useState({})
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repetirPassword, setRepetirPassword] = useState('');
 
-    const handleSubmit = async e => {
-        e.preventDefault();
+  const [alerta, setAlerta] = useState({})
 
-        if([nombre, email, password, repetirPassword].includes('')) {
-            setAlerta({ msg: 'Hay campos vacios', error: true })
-            return;
-        }
-
-        if(password !== repetirPassword) {
-            setAlerta({ msg: 'Los Password no son iguales', error: true })
-            return
-        }
-
-        if(password.length < 6) {
-            setAlerta({ msg: 'El Password es muy corto, agrega minimo 6 caracteres', error: true })
-            return
-        }
-
-        setAlerta({})
-
-        // Crear el usuario en la api
-        try {
-            await clienteAxios.post('/veterinarios', { nombre, email, password })
-            setAlerta({
-                msg: 'Creado Correctamente, revisa tu email',
-                error: false
-            })
-        } catch (error) {
-            setAlerta({
-                msg: error.response.data.msg,
-                error: true
-            })
-        }
-
+  const handleSubmit = async e => {
+    e.preventDefault();
+    if ([nombre, email, password, repetirPassword].includes('')) {
+      setAlerta({ msg: 'Hay campos vacíos', error: true })
+      return;
     }
+    if (password !== repetirPassword) {
+      setAlerta({ msg: 'Las contraseñas no son iguales', error: true })
+      return;
+    }
+    if (password.length < 6) {
+      setAlerta({ msg: 'Introduce un mínimo de 6 caracteres', error: true })
+      return;
+    }
+    setAlerta({}); //Vaciamos alerta si no hay errores
 
-    const { msg } = alerta
+    //Crear usuario en la API
+    try {
+      await clienteAxios.post('/veterinarios', { nombre, email, password }) //Creamos objeto con los valores del formulario
+      setAlerta({
+        msg: 'Usuario creado correctamente, revisa tu email',
+        error: false
+      });
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg, //Obtener acceso a los mensajes que hemos declarado en el backend
+        error: true
+      })
+    }
+  }
+  //Cargar el componente de manera condicional
+  const { msg } = alerta
 
-    return (
-        <>
-            <div>
-                <h1 className="text-indigo-600 font-black text-6xl">
-                    Crea tu Cuenta y Administra {""} 
-                    <span className="text-black">tus Pacientes</span>
-                </h1>
-            </div>
+  return (
+    <>
+      <h1 className="text-indigo-600 font-black text-6xl">Crea tu cuenta y Administra <span className="text-black">tus Pacientes</span> </h1>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        {msg && <Alerta alerta={alerta} />}
+        <form onSubmit={handleSubmit}>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">Nombre</label>
+            <input type="text" placeholder="Tu Nombre" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={nombre} onChange={e => setNombre(e.target.value)} />
+          </div>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">Email</label>
+            <input type="email" placeholder="Email" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={email} onChange={e => setEmail(e.target.value)} />
+          </div>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">Contraseña</label>
+            <input type="password" placeholder="Tu contraseña" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={password} onChange={e => setPassword(e.target.value)} />
+          </div>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">Repetir contraseña</label>
+            <input type="password" placeholder="Repetir contraseña" className="border w-full p-3 mt-3 bg-gray-50 rounded-xl" value={repetirPassword} onChange={e => setRepetirPassword(e.target.value)} />
+          </div>
+          <input type="submit" value="Crear Cuenta" className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto" />
 
-            <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+        </form>
+        <nav className="mt-10 lg:flex lg:justify-between">
+          <Link className="block text-center my-5 text-gray-500" to="/">¿Ya tienes una cuenta? Inicia Sesión</Link>
+          <Link className="block text-center my-5 text-gray-500" to="/reset-password">Olvidé mi contraseña</Link>
+        </nav>
+      </div>
+    </>
+  )
+}
 
-                { msg && <Alerta 
-                    alerta={alerta}
-                />}
-                <form 
-                    onSubmit={handleSubmit}
-                >
-                    <div className="my-5">
-                        <label
-                            className="uppercase text-gray-600 block text-xl font-bold"
-                        >
-                            Nombre
-                        </label>
-                        <input 
-                            type="text"
-                            placeholder="Tu Nombre"
-                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
-                            value={nombre}
-                            onChange={ e => setNombre(e.target.value)}
-                        />
-                    </div>
+export default Registrar
 
-                    <div className="my-5">
-                        <label
-                            className="uppercase text-gray-600 block text-xl font-bold"
-                        >
-                            Email
-                        </label>
-                        <input 
-                            type="email"
-                            placeholder="Email de Registro"
-                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
-                            value={email}
-                            onChange={ e => setEmail(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="my-5">
-                        <label
-                            className="uppercase text-gray-600 block text-xl font-bold"
-                        >
-                            Password
-                        </label>
-                        <input 
-                            type="password"
-                            placeholder="Tu Password"
-                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
-                            value={password}
-                            onChange={ e => setPassword(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="my-5">
-                        <label
-                            className="uppercase text-gray-600 block text-xl font-bold"
-                        >
-                            Repetir Password
-                        </label>
-                        <input 
-                            type="password"
-                            placeholder="Repite tu Password"
-                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
-                            value={repetirPassword}
-                            onChange={ e => setRepetirPassword(e.target.value)}
-                        />
-                    </div>
-
-
-                    <input 
-                        type="submit"
-                        value="Crear Cuenta"
-                        className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto "
-                    />
-
-                </form>
-
-                <nav className='mt-10 lg:flex lg:justify-between'>
-                    <Link 
-                        className='block text-center my-5 text-gray-500'
-                        to="/">¿Ya tienes una cuenta? Inicia Sesión</Link >
-                    <Link 
-                        className='block text-center my-5 text-gray-500'
-                        to="/olvide-password">Olvide mi Password</Link >
-                </nav>
-
-            </div>
-
-            
-        </>
-    )
-  };
   
-  export default Registrar;
