33e26378ccf8db6e025a5efd47e3d0d8ecaf5517
josefdzbeta <jafb19@outlook.com>
2022-12-29 03:08:19 +0100

    completed editing profile

--- frontend/src/paginas/EditarPerfil.jsx
+++ frontend/src/paginas/EditarPerfil.jsx
@@ -45,20 +45,20 @@ const EditarPerfil = () => {
 
                     <div className="my-3">
                         <label className="uppercase font-bold text-gray-600">Nombre</label>
-                        <input type="text" className="border bg-gray-50 w-full p-2 ,t-5 rounded-lg" name="nombre" value={perfil.nombre || ''} onChange={ e =>setPerfil({...perfil, [e.target.name] : e.target.value})} />
+                        <input type="text" className="border bg-gray-50 w-full p-2 mt-5 rounded-lg" name="nombre" value={perfil.nombre || ''} onChange={ e =>setPerfil({...perfil, [e.target.name] : e.target.value})} />
                     </div>
                     
                     <div className="my-3">
                         <label className="uppercase font-bold text-gray-600">Sito web</label>
-                        <input type="text" className="border bg-gray-50 w-full p-2 ,t-5 rounded-lg" name="web" value={perfil.web || ''} onChange={ e =>setPerfil({...perfil, [e.target.name] : e.target.value})} />
+                        <input type="text" className="border bg-gray-50 w-full p-2 mt-5 rounded-lg" name="web" value={perfil.web || ''} onChange={ e =>setPerfil({...perfil, [e.target.name] : e.target.value})} />
                     </div>
                     <div className="my-3">
                         <label className="uppercase font-bold text-gray-600">Teléfono</label>
-                        <input type="text" className="border bg-gray-50 w-full p-2 ,t-5 rounded-lg" name="telefono" value={perfil.telefono || ''} onChange={ e =>setPerfil({...perfil, [e.target.name] : e.target.value})}/>
+                        <input type="text" className="border bg-gray-50 w-full p-2 mt-5 rounded-lg" name="telefono" value={perfil.telefono || ''} onChange={ e =>setPerfil({...perfil, [e.target.name] : e.target.value})}/>
                     </div>
                     <div className="my-3">
                         <label className="uppercase font-bold text-gray-600">Email</label>
-                        <input type="text" className="border bg-gray-50 w-full p-2 ,t-5 rounded-lg" name="email" value={perfil.email || ''} onChange={ e =>setPerfil({...perfil, [e.target.name] : e.target.value})}/>
+                        <input type="text" className="border bg-gray-50 w-full p-2 mt-5 rounded-lg" name="email" value={perfil.email || ''} onChange={ e =>setPerfil({...perfil, [e.target.name] : e.target.value})}/>
                     </div>
                     <input type="submit" value="Guardar Cambios" className="bg-indigo-700 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5"/>
                 </form>
